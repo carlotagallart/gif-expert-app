247ac2dbeae2665f56c61a826888df6eeec3f479
Carlota <carlota.gallart@gmail.com>
2023-02-23 12:09:56 +0100

    add new category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react"
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState([ 'One Puch', 'Adeu Puch' ]);
-    console.log(categories);
+    const [categories, setCategories] = useState([ 'Mr. Bean', 'Harry Potter' ]);
+    const onAddCategory = () =>{
+        setCategories([  'Disney', ...categories ])
+        // setCategories( cat= [...categories, 'Disney'] )
+    }
 
     return(
         <>
 
         <h1>GifExpertApp</h1>
-
+        <button onClick={onAddCategory}>Agregar</button>
         <ol>
             { categories.map( category => {
                 return <li key={ category }>{category}</li>
